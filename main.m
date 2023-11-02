655332b5d44f28746c12fdb2d7b512a9bde1c97f
falarion08 <barrion.ryanonil@gmail.com>
2023-11-03 00:51:18 +0800

    initial implementation of midtread quantizer

--- main.m
+++ main.m
@@ -1,4 +1,34 @@
 %Read audio file
 
 [audio, audioFs] = audioread('Secret-Message.mp3');
-sound(audio,44800); 
+% sound(audio,audioFs *1); 
+
+% Stores quantization step-size or bit-depth
+% n = input("Input bit-depth: ");
+n = 3; 
+
+yq = length(audio);
+
+
+function yq = midTreadQuintizer(n,audio)
+    xMin = min(audio);
+    xMax = max(audio); 
+
+    qStep = (xMax(1) - xMin(1)) / (2^n - 1);
+    
+    audioLen = length(audio);
+    yq = zeros(audioLen,2);
+
+    for i=1:audioLen
+        xq = audio(i); 
+        j = 0; 
+        while (j <= n)
+            % % upperThresold = j + ();  
+            % lowerThreshold
+            % if ( <= j )
+            % end
+            j = j + 1; 
+        end
+    end
+end
+
